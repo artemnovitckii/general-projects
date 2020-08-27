020cc4dd3ffaba2e8b73b5dc858de8309860387f
Artem Novitckii <52527600+artemnovitckii@users.noreply.github.com>
2020-08-27 21:14:20 +1200

    Add files via upload

---
+++ Web Development/A4/MuseumShopSvc/anov099.js
@@ -0,0 +1,171 @@
+function getVersion() {
+    const xhr = new XMLHttpRequest();
+    const uri = "http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/version";
+    xhr.open("GET", uri, true);
+    xhr.onload = function () {
+       const version_d = document.getElementById("version");
+       version_d.innerHTML = "© Arty, version is " + xhr.responseText;
+    }
+    xhr.send(null);
+ }
+
+ function getNews() {
+    const uri = "http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/news";
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", uri, true);
+    xhr.setRequestHeader("Accept","Application/json")
+    xhr.onload = () => {
+       const resp = JSON.parse(xhr.responseText);
+       showNews(resp);
+    }
+    xhr.send(null);
+ }
+
+ function showNews(destinations) {
+    for (let i = 0; i < destinations.length; i++){
+        const list = destinations[i];
+        const title = list.titleField;
+        const date = list.pubDateField;
+        const description = list.descriptionField;
+        const picture = '<img src="' + list.enclosureField.urlField+ '" width="50%" height="50%">';
+        let passPage =  '<h1 id="newsHeader">'+ title + "</h1>" + '<h2 id="newsDate">' + date + "</h2>" + '<p id="newsParagraph">' + description + "</p>" + "<br/><br/>" + picture + "<br/><br/><br/>" ;
+        if (i != destinations.length - 1){
+            passPage += "<hr width=90% size=5px color=#bc6596 radius=5px>" + "<br/>";
+        }
+        document.getElementById("News").innerHTML += passPage;
+        
+    }
+ }
+
+function showHomeTab() {
+    if(currentTab != "HomeTab"){
+        currentTab = "HomeTab";
+        showNoTabs();
+        document.getElementById("HomeTab").style.backgroundColor = "#9dd3f7";
+        document.getElementById("HomePage").style.display = "inline";
+    }
+}
+
+function searchGetShopItems(){
+    const term = document.getElementById("shopInput").value;
+    const searchurl = `http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/shop?term=${term}`;
+    const xhr1 = new XMLHttpRequest();
+    xhr1.open("GET", searchurl, true);
+    xhr1.setRequestHeader("Accept","Application/json")
+    xhr1.onload = () => {
+       const items = JSON.parse(xhr1.responseText);
+       searchShowShopItems(items);
+    }
+    xhr1.send(null);
+}
+
+
+function searchShowShopItems(items){
+    console.log(items);
+    let shopItems = '';
+    for (let i = 0; i < items.length; i++){
+        const list1 = items[i];
+        const description1 = list1.Description;
+        const displayPicture =  '<img src="http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/shopimg?id=' + list1.ItemId +'" width="50%" height="50%">';
+        const title1 = list1.Title;
+        const buylink = '<a href="http://redsox.uoa.auckland.ac.nz/mss/Service.svc/buy?id=' + list1.ItemId + '">Purchase</a>';
+        let passPage =  '<h1 id="displayHeader">'+title1+"</h1>" + '<p id="newsParagraph">' + description1 + "</p>" + "</br></br>" + displayPicture + "</br></br>" + buylink + "</br></br>";
+        if (i != items.length - 1){
+            passPage += "<hr width=90% size=5px color=#bc6596 radius=5px>";
+        }
+        shopItems += passPage;
+    }
+    document.getElementById("ShopItems").innerHTML = shopItems;
+}
+
+
+
+
+function getDisplays(){
+    const url = "http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/items";
+    const xhr1 = new XMLHttpRequest();
+    xhr1.open("GET", url, true);
+    xhr1.setRequestHeader("Accept","Application/json")
+    xhr1.onload = () => {
+       const items = JSON.parse(xhr1.responseText);
+       searchShowDisplays(items);
+    }
+    xhr1.send(null);
+}
+
+function searchGetDisplay(){
+    const term = document.getElementById("myInput").value;
+    const searchurl = `http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/search?term=${term}`;
+    const xhr1 = new XMLHttpRequest();
+    xhr1.open("GET", searchurl, true);
+    xhr1.setRequestHeader("Accept","Application/json")
+    xhr1.onload = () => {
+       const items = JSON.parse(xhr1.responseText);
+       searchShowDisplays(items);
+    }
+    xhr1.send(null);
+}
+
+function searchShowDisplays(items){
+    //console.log(items);
+    let newstring = '';
+    for (let i = 0; i < items.length; i++){
+        const list1 = items[i];
+        const description1 = list1.Description;
+        const displayPicture =  '<img src="http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/itemimg?id=' + list1.ItemId +'" width="50%" height="50%">';
+        const title1 = list1.Title;
+        let passPage1 =  '<h1 id="displayHeader">'+title1+"</h1>" + '<p id="newsParagraph">' + description1 + "</p>" + "</br></br>" + displayPicture + "</br></br>";
+        if (i != items.length - 1){
+            passPage1 += "<hr width=90% size=5px color=#bc6596 radius=5px>";
+        }
+        newstring += passPage1;
+    }
+    document.getElementById("Displaying").innerHTML = newstring;
+}
+
+function postComments(){
+    const name = document.getElementById("name").value;
+    const nameurl = `http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/comment?name=${name}`;
+    const comment = document.getElementById("comment").value;
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST",nameurl,true);
+    let jsoncomment = JSON.stringify(comment);
+    xhr.setRequestHeader("Content-Type","Application/json");
+    xhr.send(jsoncomment);
+    xhr.onload = () => {
+        document.getElementById("frame").src="http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/htmlcomments";
+     }
+
+}
+
+function registerUser(){
+    const userName = document.getElementById("userName").value;
+    let userDetails = { Address : document.getElementById("userAddress").value, Name : userName, Password : document.getElementById("userPassword").value};
+    if (userName.length != 0 && (document.getElementById("userPassword").value).length != 0 && (document.getElementById("userAddress").value).length != 0){
+        const registerLink = "http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/register";
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", registerLink, true);
+        xhr.setRequestHeader("Content-Type","Application/json");
+        xhr.send(JSON.stringify(userDetails));
+        xhr.onreadystatechange = () =>{
+            if (xhr.statusText == "OK") {
+                document.getElementById("feedback").innerHTML = "User " + userName + " registered!";
+            } 
+
+            else{
+                document.getElementById("feedback").innerHTML = "User " + userName + " was not registered";
+            }
+        }
+    }
+}
+
+
+
+
+ window.onload = function () {
+     getVersion();
+     showHomeTab();
+     getNews();
+     getDisplays();
+     searchGetShopItems();
+ }
